2aafa4fafd8148abe950b97bb9f124c2409fe5bc
Yilai Li <44369625+yilaili@users.noreply.github.com>
2020-02-05 20:09:46 -0500

    debug

--- submit_ctf.py
+++ submit_ctf.py
@@ -112,7 +112,7 @@ def submit(**args):
         f.write('Job submitted. Job ID is %s.\n' %(job_id))
     query_cmd = cluster_config[cluster]['query_cmd']
     keyarg = cluster_config[cluster]['keyarg']
-    os.chdir(codedir) ## cd back to the directory of the code
+    # os.chdir(codedir) ## cd back to the directory of the code
     return job_id, query_cmd, keyarg
 
 def check_complete(job_id, query_cmd, keyarg, **args):
