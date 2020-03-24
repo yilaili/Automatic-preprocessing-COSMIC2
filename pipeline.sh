ac9c2945addfb803491ff10a487e4e8a260b0663
Yilai Li <44369625+yilaili@users.noreply.github.com>
2020-03-24 11:35:02 -0400

    change node from 2 to 1 for ctf and extraction jobs

--- pipeline.sh
+++ pipeline.sh
@@ -24,7 +24,7 @@ conda activate pipeline
 ### printf 'data_\nloop_\n_rlnMicrographName\n' >> $input_star | ls $input_dir/*.mrc >> $input_star
 
 python /home/yilaili/codes/Automatic-preprocessing-COSMIC2/submit_micassess.py -i $input_star --user_email $user_email
-python /home/yilaili/codes/Automatic-preprocessing-COSMIC2/submit_ctf.py -i micrographs_micassess.star -o ctf --CS $CS --HT $HT --DStep $apix --user_email $user_email --nodes 2
+python /home/yilaili/codes/Automatic-preprocessing-COSMIC2/submit_ctf.py -i micrographs_micassess.star -o ctf --CS $CS --HT $HT --DStep $apix --user_email $user_email --nodes 1
 python /home/yilaili/codes/Automatic-preprocessing-COSMIC2/filter_goodmic.py -i $input_dir -o good_micrographs -g micrographs_micassess.star
 
 mkdir mic_subset
@@ -37,7 +37,7 @@ size=$(head -1 findsize.txt)
 python /home/yilaili/codes/Automatic-preprocessing-COSMIC2/submit_ppicking.py -i good_micrographs -o ppicking --boxsize $size --apix $apix --user_email $user_email
 
 let extract_size=2*$size
-python /home/yilaili/codes/Automatic-preprocessing-COSMIC2/submit_extract.py -i ctf/micrographs_ctf.star --coord_dir ppicking --part_dir extract --part_star particles.star --apix $apix --extract_size $extract_size --user_email $user_email --nodes 2
+python /home/yilaili/codes/Automatic-preprocessing-COSMIC2/submit_extract.py -i ctf/micrographs_ctf.star --coord_dir ppicking --part_dir extract --part_star particles.star --apix $apix --extract_size $extract_size --user_email $user_email --nodes 1
 
 diam1=$(echo "$size*0.5" | bc)
 diam1=${diam1%.*}
@@ -60,4 +60,4 @@ python /home/yilaili/codes/Automatic-preprocessing-COSMIC2/submit_2dassess.py -i
 python /home/yilaili/codes/Automatic-preprocessing-COSMIC2/write_good_particles.py -i run_it025_data.star -c 2DClass -g good_part_frac.txt -o selected_particles.star
 mv selected_particles.star extract/
 
-python /home/yilaili/codes/Automatic-preprocessing-COSMIC2/submit_extract.py -p relion_reextract -i ctf/micrographs_ctf.star --coord_dir ppicking --part_dir reextract --part_star reextract_selected_particles.star --reextract_data_star extract/selected_particles.star --apix $apix --extract_size $extract_size --scaled_apix $final_apix --user_email $user_email --nodes 2
+python /home/yilaili/codes/Automatic-preprocessing-COSMIC2/submit_extract.py -p relion_reextract -i ctf/micrographs_ctf.star --coord_dir ppicking --part_dir reextract --part_star reextract_selected_particles.star --reextract_data_star extract/selected_particles.star --apix $apix --extract_size $extract_size --scaled_apix $final_apix --user_email $user_email --nodes 1
